07fc6d2a175133e8b52318faf2d628c6ef0704b7
Tishana Trainor <TTrainor@mathematica-mpr.com>
2020-06-30 13:23:04 -0400

    added style and minimal login function to front end

--- DonzoApp.API/Controllers/ValuesController.cs
+++ DonzoApp.API/Controllers/ValuesController.cs
@@ -21,6 +21,7 @@ namespace DonzoApp.API.Controllers
 
         }
         // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
