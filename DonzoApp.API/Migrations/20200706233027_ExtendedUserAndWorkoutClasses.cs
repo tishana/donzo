1590b74e66e99c2e034eb53bc5460353ac1c0cd0
Tishana Trainor <TTrainor@mathematica-mpr.com>
2020-07-06 20:19:51 -0400

    created seed data and Seed.cs and modified Program.cs to create db

---
+++ DonzoApp.API/Migrations/20200706233027_ExtendedUserAndWorkoutClasses.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DonzoApp.API.Migrations
+{
+    public partial class ExtendedUserAndWorkoutClasses : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Moods",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Moods", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WorkoutTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WorkoutTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Workouts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    DateCompleted = table.Column<DateTime>(nullable: false),
+                    WorkoutTypeId = table.Column<int>(nullable: false),
+                    Duration = table.Column<int>(nullable: false),
+                    MoodId = table.Column<int>(nullable: false),
+                    UserId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Workouts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Workouts_Moods_MoodId",
+                        column: x => x.MoodId,
+                        principalTable: "Moods",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Workouts_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Workouts_WorkoutTypes_WorkoutTypeId",
+                        column: x => x.WorkoutTypeId,
+                        principalTable: "WorkoutTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Workouts_MoodId",
+                table: "Workouts",
+                column: "MoodId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Workouts_UserId",
+                table: "Workouts",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Workouts_WorkoutTypeId",
+                table: "Workouts",
+                column: "WorkoutTypeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Workouts");
+
+            migrationBuilder.DropTable(
+                name: "Moods");
+
+            migrationBuilder.DropTable(
+                name: "WorkoutTypes");
+        }
+    }
+}
--- DonzoApp.API/Models/Workout.cs
+++ DonzoApp.API/Models/Workout.cs
@@ -6,10 +6,11 @@ namespace DonzoApp.API.Models
     {
         public int Id { get; set; }
         public DateTime DateCompleted { get; set; }
-        public WorkoutType WTypeId { get; set; }
+        public WorkoutType WorkoutType { get; set; }
+        public int WorkoutTypeId { get; set; }
         public int Duration { get; set; }
-        public Mood MoodId { get; set; }
-        public User UserId { get; set; }
+        public Mood Mood { get; set; }
+        public int MoodId { get; set; }
 
     }
 }
