c062a11033d1a08439b32f3c16bf0a657f74a785
Tishana <tishana.trainor@gmail.com>
2020-08-31 06:33:23 -0400

    added mat card

--- DonzoApp-SPA/src/app/nav/nav.component.spec.ts
+++ DonzoApp-SPA/src/app/nav/nav.component.spec.ts
@@ -1,28 +1,25 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { NavComponent } from './nav.component';
+import { NavComponent } from './nav.component'
 
 describe('NavComponent', () => {
-  let component: NavComponent;
-  let fixture: ComponentFixture<NavComponent>;
+  let component: NavComponent
+  let fixture: ComponentFixture<NavComponent>
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NavComponent ]
-    })
-    .compileComponents();
-  }));
+      declarations: [NavComponent],
+    }).compileComponents()
+  }))
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(NavComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture = TestBed.createComponent(NavComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+})
